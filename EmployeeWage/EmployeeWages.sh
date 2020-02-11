2eb9fc12fef1f79aaa29edfa6d218147114de743
janardan1995 <dasjanardan1995@gmail.com>
2020-02-11 19:06:23 +0530

    [JD]Refactor.Employee display message by creating Uc4 branch on the remoterepository

--- EmployeeWage/EmployeeWages.sh
+++ EmployeeWage/EmployeeWages.sh
@@ -2,18 +2,15 @@
 echo Welcome to Employee Wage Computation Program on Master Branch
 x=$(($RANDOM%2))
 echo $x
-if(($x==0))
-then 
-echo employ is present
-#if he present then he works for 8 hour per 20 rupees
-wage=$((20*8))
-#if he wanna do extra work
-#noted that *hour should have completed
-echo how many extra hour that he works
+case $x in
+1) echo how many extra hour that he works
 read e
-extraIncome=$(($e * 20))
-echo his extra inome is : $extraIncome
-echo his income $wage
-else
-echo employee is absent
-fi
+extra=$(($extra+$e))
+wage=$(($wage+$(($((20*8))+$(($e*20))))))
+count=$(($count+1))
+;;
+0) echo  absent
+;;
+esac
+echo $wage
+echo $extra
