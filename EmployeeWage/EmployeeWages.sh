cc1fdf03d0bafed8a76ce9aba18f760580f6b2ea
janardan1995 <dasjanardan1995@gmail.com>
2020-02-11 18:42:40 +0530

    [JD]Refactor.Employee display message by creating Uc1 branch on the remoterepository

--- EmployeeWage/EmployeeWages.sh
+++ EmployeeWage/EmployeeWages.sh
@@ -1,2 +1,10 @@
 #!/bin/bash/ -x
 echo Welcome to Employee Wage Computation Program on Master Branch
+x=$(($RANDOM%2))
+echo $x
+if(($x==0))
+then 
+echo employ is present
+else
+echo employee is absent
+fi
