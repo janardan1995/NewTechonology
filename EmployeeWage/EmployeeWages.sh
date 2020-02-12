0e96f122b282f6572553db03e74e3fbf18cfba20
janardan1995 <dasjanardan1995@gmail.com>
2020-02-12 18:19:37 +0530

    [JD]refactor.branch uc7

--- EmployeeWage/EmployeeWages.sh
+++ EmployeeWage/EmployeeWages.sh
@@ -1,22 +1,6 @@
 totalworkHour()
 {
-int=$1
-echo wages for a month
-# if  calculiting 20 working day
-count=1
-workDay=20
-extra=0
-hour=0
-while [ $workDay != 0 ] && [ $hour -le 100 ]
-do
-echo $count
-wage=$(($wage+$((20*8))))
-workDay=$(($workDay-1))
 hour=$(($hour+$1))
-count=$(($count+1))
-done
-echo total wages : $wage
-echo $hour
+return $hour
 }
-
-totalworkHour 4
+echo $?
