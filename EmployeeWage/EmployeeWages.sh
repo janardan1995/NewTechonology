8429e398277f35b11f318b5fb472b553ff85046d
janardan1995 <dasjanardan1995@gmail.com>
2020-02-12 11:05:24 +0530

    [JD]Refactor.uc6(Calculate Wages till) branch on the remoterepository

--- EmployeeWage/EmployeeWages.sh
+++ EmployeeWage/EmployeeWages.sh
@@ -1,13 +1,18 @@
+#!/bin/sh -x
 echo wages for a month
 # if  calculiting 20 working day
-count=0
+count=1
 workDay=20
 extra=0
-
-while(($workDay!=0))
+hour=0
+while [ $workDay != 0 ] && [ $hour -le 100 ]
 do
+echo $count
 wage=$(($wage+$((20*8))))
 workDay=$(($workDay-1))
+hour=$(($hour+8))
+count=$(($count+1))
 done
 echo total wages : $wage
-
+echo $hour
+echo perfect!!
