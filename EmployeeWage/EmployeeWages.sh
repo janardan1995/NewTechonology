76238a06dad4f2af6e172bcf063c540ef32f3fb0
janardan1995 <dasjanardan1995@gmail.com>
2020-02-12 16:05:32 +0530

    [JD]Refactor. update branch uc5 such that it can solve the merge conflicts

--- EmployeeWage/EmployeeWages.sh
+++ EmployeeWage/EmployeeWages.sh
@@ -1,13 +1,37 @@
-echo wages for a month
-# if  calculiting 20 working day
+#!/bin/bash/ -x
+echo "Welcome to Employee Wage Computation Program"
 count=0
 workDay=20
 extra=0
 
 while(($workDay!=0))
 do
-wage=$(($wage+$((20*8))))
+x=$(($RANDOM%2))
+echo $x
+
+if(($x==0))
+then
+echo for full time press 1
+echo for part time press 2
+read y
+case $y in
+
+1) echo "he wanna work full time"
+        FullTimewage=$(($FullTimewage+$((20*8))))
+;;
+
+2) echo "he wanna work part time for 4 hours per day"
+       PartTimewage=$(($PartTimewage+$((20*4))))
+;;
+
+*) echo invalide input
+esac
+
+else
+echo he is absent
+fi
 workDay=$(($workDay-1))
 done
-echo total wages : $wage
 
+echo full time wage :$FullTimewage
+echo PartTimewage :$PartTimewage
