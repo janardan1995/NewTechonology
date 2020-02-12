1bd28acd4d70fa0d24d8a4eb2221b3b2e8477ff3
janardan1995 <dasjanardan1995@gmail.com>
2020-02-12 09:53:27 +0530

    [JD]Refactor.uc2 branch on the remoterepository

--- EmployeeWage/EmployeeWages.sh
+++ EmployeeWage/EmployeeWages.sh
@@ -1,13 +1,4 @@
-#!/bin/bash/ -x
-echo Welcome to Employee Wage Computation Program on Master Branch
-x=$(($RANDOM%2))
-echo $x
-if(($x==0))
-then 
-echo employ is present
-#if he present then he works for 8 hour per 20 rupees
+#if he present then he works for 8 hour per hour he earns 20 rupees
 wage=$((20*8))
 echo $wage
-else
-echo employee is absent
-fi
+
