5f6b482c753a3fdbe594d4cbed6eca48a078fa6a
janardan1995 <dasjanardan1995@gmail.com>
2020-02-12 19:35:52 +0530

    [JD]refactor. branch uc8 where storing the wages in a file

--- EmployeeWage/EmployeeWages.sh
+++ EmployeeWage/EmployeeWages.sh
@@ -1,22 +1,66 @@
+#!/bin/bash/ -x
 totalworkHour()
 {
-int=$1
-echo wages for a month
-# if  calculiting 20 working day
+if(($1!=0))
+then
+hour=$(($hour+$1))
+return $hour
+else
+echo total hours employee work in that month: $hour
+fi
+}
+
+
+echo "Welcome to Employee Wage Computation Program"
+count=0
+# if  calculiting 20 working day and total hour that employee works less then 100
 count=1
 workDay=20
-extra=0
+
 hour=0
 while [ $workDay != 0 ] && [ $hour -le 100 ]
 do
-echo $count
-wage=$(($wage+$((20*8))))
-workDay=$(($workDay-1))
-hour=$(($hour+$1))
-count=$(($count+1))
-done
-echo total wages : $wage
-echo $hour
-}
+x=$(($RANDOM%2))
+DailyWage=0
+echo $x
+if(($x==0))
+then
+echo for full time press 1
+echo for part time press 2
+read y
+case $y in
+1) echo "he wanna work full time"
+        wage=$(($wage+$((20*8))))
+	FullTimewage=$((20*8))
+        DailyWage=$(($DailyWage+1))
+        echo $FullTimewage  >> test.txt
+        totalworkHour 4
 
-totalworkHour 4
+;;
+
+2) echo "he wanna work part time for 4 hours per day"
+       wage=$(($wage+$((20*4))))
+       PartTimewage=$((20*4))
+       DailyWage=$(($DailyWage+1))
+       echo $PartTimewage  >> test.txt 
+       totalworkHour 8
+
+;;
+
+*) echo invalide input
+esac
+else
+ echo he is absent
+  
+  echo 0  >> test.txt 
+fi
+ echo $count
+ wage=$(($wage+$((20*8))))
+ workDay=$(($workDay-1))
+ count=$(($count+1))
+ done
+ 
+ 
+ totalworkHour 0
+ echo total wages is $wage >> test.txt
+
