bafdd4e84c535049b8f61631313deec6c1c90730
janardan1995 <dasjanardan1995@gmail.com>
2020-02-13 19:23:43 +0530

    [JD]Add. create a new branch uc-2 and push it to the remote repository

--- SnakeAndLadder/SnakeAndLadder.sh
+++ SnakeAndLadder/SnakeAndLadder.sh
@@ -2,4 +2,8 @@
 echo WECOME TO SNAKE AND LADDER
 echo single player playeing the game and his starting position is 0
 CountUser=0
-
+#random function for randomly generated number
+random(){
+ x=$(($(($RANDOM%6))+1))
+return $x
+}
