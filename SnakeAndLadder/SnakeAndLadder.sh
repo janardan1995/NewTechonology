65f25ced48230b70a352c5ca433898fcb10d5594
janardan1995 <dasjanardan1995@gmail.com>
2020-02-15 10:07:59 +0530

    [JD]Refactor. updating the file

--- SnakeAndLadder/SnakeAndLadder.sh
+++ SnakeAndLadder/SnakeAndLadder.sh
@@ -1,5 +1,5 @@
-#!/bin/bash/ -x
-echo WECOME TO SNAKE AND LADDER
+
+
 echo single player playeing the game and his starting position is 0
 CountUser=0
 #random function for randomly generated number
