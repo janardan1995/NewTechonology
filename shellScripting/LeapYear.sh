21a6d9ea86248e5fdc45fd481658b36ca299a1c3
janardan1995 <dasjanardan1995@gmail.com>
2020-02-17 10:45:41 +0530

    [JD]Add.Leap year program to the remote repository

---
+++ shellScripting/LeapYear.sh
@@ -0,0 +1,19 @@
+#!/bin/bash/ -x
+echo "enter the year to know leap year"
+read y
+year=$y
+a=$(($y%4))
+b=$(($y%400))
+c=$(($y%100))
+
+if [ $year -ge 1582 -a $year -le 9999 ]
+then
+ if [ $a -eq 0 -a $c -ne 0 -o $b -eq 0  ]
+  then
+     echo "leap year"
+  else
+     echo "it's not a leap year"
+ fi
+else
+echo "you have entered an invalid year"
+fi
