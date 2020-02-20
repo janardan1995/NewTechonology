cde81e579971f8876124747b7d45935766bbc1ce
janardan1995 <dasjanardan1995@gmail.com>
2020-02-20 18:53:03 +0530

    [JD]Refactor. update branch name uc5

--- EmployeeWage/EmployeeWages.sh
+++ EmployeeWage/EmployeeWages.sh
@@ -3,19 +3,16 @@ echo "Welcome to Employee Wage Computation Program"
 count=0
 workDay=20
 extra=0
-
 while(($workDay!=0))
 do
 x=$(($RANDOM%2))
 echo $x
-
 if(($x==0))
 then
 echo for full time press 1
 echo for part time press 2
 read y
 case $y in
-
 1) echo "he wanna work full time"
         FullTimewage=$(($FullTimewage+$((20*8))))
 ;;
@@ -26,12 +23,10 @@ case $y in
 
 *) echo invalide input
 esac
-
 else
 echo he is absent
 fi
 workDay=$(($workDay-1))
 done
-
 echo full time wage :$FullTimewage
 echo PartTimewage :$PartTimewage
