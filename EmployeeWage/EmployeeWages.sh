f59e57d83ef9132502fc513bbe8e3601575cb81d
janardan1995 <dasjanardan1995@gmail.com>
2020-02-20 19:53:43 +0530

    [JD]Refactor. update uc9 branch

--- EmployeeWage/EmployeeWages.sh
+++ EmployeeWage/EmployeeWages.sh
@@ -1,4 +1,6 @@
 #!/bin/bash/ -x
+
+#by using this function we can we can know the total hour
 totalworkHour()
 {
 if(($1!=0))
@@ -6,7 +8,7 @@ then
 hour=$(($hour+$1))
 return $hour
 else
-echo total hours employee work in that month: $hour
+echo "total hours employee work in that month: $hour"
 fi
 }
 
@@ -16,22 +18,30 @@ count=0
 # if  calculiting 20 working day and total hour that employee works less then 100
 count=1
 workDay=20
-
+DailyWage=0
 hour=0
+
+#while loop is used such that total working day should less then 20 days and total working hour should less then 100 hours
 while [ $workDay != 0 ] && [ $hour -le 100 ]
 do
+
+#here random function is used for randomly choose absent and present
 x=$(($RANDOM%2))
-DailyWage=0
 echo $x
-if(($x==0))
+if(($x==1))
 then
-echo for full time press 1
-echo for part time press 2
+
+#here user can choose while emploee working in part time or fulltime work
+#for part time employee can work for 7 hour
+#for full time employee should work for 10 hour
+
+echo "for full time press 1"
+echo "for part time press 2"
 read y
 case $y in
 1) echo "he wanna work full time"
-        wage=$(($wage+$((20*8))))
-	FullTimewage=$((20*8))
+        wage=$(($wage+$((20*10))))
+	FullTimewage=$((20*10))
         DailyWage=$(($DailyWage+1))
         echo $FullTimewage for day $DailyWage >> test.txt
         totalworkHour 4
@@ -39,28 +49,27 @@ case $y in
 ;;
 
 2) echo "he wanna work part time for 4 hours per day"
-       wage=$(($wage+$((20*4))))
-       PartTimewage=$((20*4))
+       wage=$(($wage+$((20*7))))
+       PartTimewage=$((20*7))
        DailyWage=$(($DailyWage+1))
        echo $PartTimewage for day $DailyWage >> test.txt 
        totalworkHour 8
 
 ;;
 
-*) echo invalide input
+*) echo invalid input
 esac
 else
  echo he is absent
   DailyWage=$(($DailyWage+1))
-  echo 0 for day $DailyWage >> test.txt 
+  echo 0 for day $DailyWage >> test.txt
 fi
  echo $count
  wage=$(($wage+$((20*8))))
  workDay=$(($workDay-1))
  count=$(($count+1))
  done
- 
- 
+
  totalworkHour 0
  echo total wages is $wage >> test.txt
-
+echo total working hour $?
