f1ff16c62f25bdcf98030c1bf51e6289b67f5d01
janardan1995 <dasjanardan1995@gmail.com>
2020-02-20 17:28:26 +0530

    [JD]Refactor. update uc2 branch and puch it to remote repository

--- EmployeeWage/EmployeeWages.sh
+++ EmployeeWage/EmployeeWages.sh
@@ -1,3 +1,12 @@
+#!/bin/bash/ -x
+echo "welcome to Employwages program"
+x=$(($RANDOM%2))
+if(($x==0))
+then
+echo employ is present
+else
+echo employee is absent
+fi
 #if he present then he works for 8 hour per hour he earns 20 rupees
 wage=$((20*8))
 echo $wage
