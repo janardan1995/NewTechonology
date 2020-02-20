13435b6994c0c80d96e443129a6bfc3984817895
janardan1995 <dasjanardan1995@gmail.com>
2020-02-20 17:52:58 +0530

    [JD]Refactor. update uc3 branch

--- EmployeeWage/EmployeeWages.sh
+++ EmployeeWage/EmployeeWages.sh
@@ -1,4 +1,15 @@
-
+#!/bin/bash/ -x
+echo "welcome to Employwages program"
+x=$(($RANDOM%2))
+if(($x==0))
+then
+echo employ is present
+else
+echo employee is absent
+fi
+#if he present then he works for 8 hour per hour he earns 20 rupees
+wage=$((20*8))
+echo $wage
 #if he wanna do part time work
 #assuming that part time hour is 4 hour as full time is 8
 
